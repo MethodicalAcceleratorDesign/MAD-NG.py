52e740288379d7dbc122f1459cdbcb680e4b8821
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-13 12:03:15 +0200

    Adjusted for simplified pipe

--- examples/ex-FODO/FODOs.py
+++ examples/ex-FODO/FODOs.py
@@ -1,7 +1,8 @@
+from xml.sax.handler import LexicalHandler
 from pymadng import MAD
 import matplotlib.pyplot as plt
 
-with MAD() as mad:
+with MAD(log=True) as mad:
     mad.loadsequence("seq", "fodo.seq", "fodo.mad")
     mad.beam("beam1")
     mad.seq.beam = mad.beam1
@@ -14,25 +15,28 @@ with MAD() as mad:
 
 with MAD() as mad:
     mad["circum", "lcell"] = 60, 20
-    mad.sendVariables(["circum", "lcell"])
 
     mad.importVariables("math", ["sin", "pi"])
-    mad.deferred("v", k = "1/(lcell/sin(pi/4)/4)")
+    mad.deferred("v", k="1/(lcell/sin(pi/4)/4)")
 
     mad.quadrupole("qf", mad.defExpr(knl=[0, " v.k"]), l=1)
     mad.quadrupole("qd", mad.defExpr(knl=[0, "-v.k"]), l=1)
-    mad.sequence("seq",
-        mad.qf.set(at=0   * mad.lcell),
+    mad.sequence(
+        "seq",
+        mad.qf.set(at=0 * mad.lcell),
         mad.qd.set(at=0.5 * mad.lcell),
-        mad.qf.set(at=1   * mad.lcell),
+        mad.qf.set(at=1 * mad.lcell),
         mad.qd.set(at=1.5 * mad.lcell),
-        mad.qf.set(at=2   * mad.lcell),
+        mad.qf.set(at=2 * mad.lcell),
         mad.qd.set(at=2.5 * mad.lcell),
-        refer="entry", l=mad.circum,
+        refer="entry",
+        l=mad.circum,
     )
     mad.beam("beam1")
     mad.seq.beam = mad.beam1
-    mad.twiss("mtbl", sequence=mad.seq, method=4, nslice=10, implicit=True, save="atbody")
+    mad.twiss(
+        "mtbl", sequence=mad.seq, method=4, nslice=10, implicit=True, save="atbody"
+    )
     cols = ["name", "s", "beta11", "beta22", "mu1", "mu2", "alfa11", "alfa22"]
     mad.callMethod(None, "mtbl", "write", "'twiss_py.tfs'", cols)
 
@@ -42,7 +46,8 @@ with MAD() as mad:
     plt.ylabel("beta11")
     plt.show()
 with MAD() as mad:
-    sharedData = mad.sendScript("""
+    sharedData = mad.sendScript(
+        """
     local beam, twiss in MAD
     MADX:load("fodo.seq", "fodo.mad")
     local seq in MADX
@@ -51,7 +56,8 @@ with MAD() as mad:
     mtbl = twiss {sequence=seq, method=4, implicit=true, nslice=10, save="atbody"}
     mtbl:write("twiss_mad_tfs", cols)
     sharedata({mtbl})
-    """)
+    """
+    )
     mad.mtbl = sharedData[0]
     mad.mtbl.__name__ = "mtbl"
     plt.plot(mad.mtbl.s, mad.mtbl["beta11"])
@@ -59,7 +65,8 @@ with MAD() as mad:
 
 
 with MAD() as mad:
-    mad.sendScript("""
+    mad.sendScript(
+        """
     local beam, twiss in MAD
     MADX:load("fodo.seq", "fodo.mad")
     local seq in MADX
@@ -67,7 +74,8 @@ with MAD() as mad:
     local cols = {'name', 's', 'beta11', 'beta22', 'mu1', 'mu2', 'alfa11', 'alfa22'}
     mtbl = twiss {sequence=seq, method=4, implicit=true, nslice=10, save="atbody"}
     mtbl:write("twiss_mad_tfs", cols)
-    """)
+    """
+    )
     mad.importVariables("_G", ["mtbl"])
     plt.plot(mad.mtbl.s, mad.mtbl["beta11"])
     plt.show()
