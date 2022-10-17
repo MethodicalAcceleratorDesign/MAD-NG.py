c7115d6afab0cc50c208e6f5e9d19df9c351758d
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-17 14:25:03 +0200

    Improved name scheme and other minor adjustments

--- src/pymadng/madProc.py
+++ src/pymadng/madProc.py
@@ -1,41 +1,43 @@
 import tempfile, os, subprocess, sys
 import numpy as np
 
-#Working: mad.send("""send([==[self.send('''send([=[self.send("send([[print('hello world')]])")]=])''')]==])""")
-#Working: mad.send("""send([=[self.send("send([[print('hello')]])")]=])""")
+#Working: mad.send("""MAD.send([==[mad.send('''MAD.send([=[mad.send("MAD.send([[print('hello world')]])")]=])''')]==])""")
+#Working: mad.send("""MAD.send([=[mad.send("MAD.send([[print('hello')]])")]=])""")
 
 class madProcess:
     globalVars = {"np": np}
 
-    def __init__(self) -> None:
+    def __init__(self, className: str = "mad", pathToMAD: str = None) -> None:
+        self.className = className
         self.tmpFldr = tempfile.TemporaryDirectory(prefix="pymadng-")
         self.pipeDir = self.tmpFldr.name + "/pipe"
         os.mkfifo(self.pipeDir)
-
+        
+        pathToMAD = pathToMAD or os.path.dirname(os.path.abspath(__file__)) + "/mad"
         self.process = subprocess.Popen(
-            [os.path.dirname(os.path.abspath(__file__)) + "/mad",  "-q",  "-i"],
+            [pathToMAD,  "-q",  "-i"],
             bufsize=0,
             stdout=sys.stdout,
             stderr=sys.stdout,
             stdin=subprocess.PIPE,
+            text=True
         )
         self.send(f"""
-        send = require ("madl_buffer").send
-        local openPipe in require("madl_buffer")
-        openPipe("{self.pipeDir}")
+        MAD.send = require ("madl_buffer").send
+        local openPipeToPython in require("madl_buffer")
+        openPipeToPython("{self.pipeDir}")
         """)
 
-        self.pipe = os.open(self.pipeDir, os.O_RDONLY)
+        self.MADToPyPipe = os.open(self.pipeDir, os.O_RDONLY)
         self.send("_PROMPT = ''") #Change this to change how output works
 
     def send(self, input: str) -> int:
-        self.process.stdin.write(("load([==========[" + input + "]==========])()\n").encode("utf-8"))
-        self.process.stdin.flush()
+        self.process.stdin.write(("load([==========[" + input + "]==========])()\n"))
     
     def read(self):
         pipeText = os.read(self.pipe, 8192)
         code = compile(pipeText, "pipe", "exec")
-        exec(code, self.globalVars, {"self": self})
+        exec(code, self.globalVars, {self.className: self})
 
     def __del__(self):
         self.tmpFldr.cleanup()
