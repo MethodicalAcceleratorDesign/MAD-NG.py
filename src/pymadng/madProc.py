c92c6e4b2b769fd0eaad69f8d76c66ef4671c761
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-17 09:38:32 +0200

    remove polling of pipe

--- src/pymadng/madProc.py
+++ src/pymadng/madProc.py
@@ -1,4 +1,4 @@
-import tempfile, select, os, subprocess, sys
+import tempfile, os, subprocess, sys
 import numpy as np
 
 #Working: mad.send("""send([==[self.send('''send([=[self.send("send([[print('hello world')]])")]=])''')]==])""")
@@ -8,8 +8,8 @@ class madProcess:
     globalVars = {"np": np}
 
     def __init__(self) -> None:
-        self.__tmpFldr = tempfile.TemporaryDirectory(prefix="pymadng-")
-        self.pipeDir = self.__tmpFldr.name + "/pipe"
+        self.tmpFldr = tempfile.TemporaryDirectory(prefix="pymadng-")
+        self.pipeDir = self.tmpFldr.name + "/pipe"
         os.mkfifo(self.pipeDir)
 
         self.process = subprocess.Popen(
@@ -26,24 +26,18 @@ class madProcess:
         """)
 
         self.pipe = os.open(self.pipeDir, os.O_RDONLY)
-        self.pollIn = select.poll()
-        self.pollIn.register(self.pipe, select.POLLIN)
         self.send("_PROMPT = ''") #Change this to change how output works
 
     def send(self, input: str) -> int:
         self.process.stdin.write(("load([==========[" + input + "]==========])()\n").encode("utf-8"))
         self.process.stdin.flush()
     
-    def readPipe(self, timeout = 60):
-        """Read the pipe, timing out after 'timeout' seconds."""
-        if self.pollIn.poll(1000*timeout) == []:  # 1 Minute poll!
-            raise(TimeoutError("Mad has not sent anything"))
-        else:
-            pipeText = os.read(self.pipe, 8192)
-            code = compile(pipeText, "pipe", "exec")
-            exec(code, self.globalVars, {"self": self})
+    def readPipe(self):
+        pipeText = os.read(self.pipe, 8192)
+        code = compile(pipeText, "pipe", "exec")
+        exec(code, self.globalVars, {"self": self})
 
     def __del__(self):
-        self.__tmpFldr.cleanup()
+        self.tmpFldr.cleanup()
         self.process.terminate()
         self.process.wait()
