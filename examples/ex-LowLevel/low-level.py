b17ebae0b1114ed01197295a00973b98fc08e572
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-21 10:13:02 +0200

    Added cmatrix and vector examples

--- examples/ex-LowLevel/low-level.py
+++ examples/ex-LowLevel/low-level.py
@@ -1,17 +1,45 @@
 from pymadng import MAD
 import numpy as np
+import time
 
 mad = MAD()
+
 mad.process.send("""
-    local function madMatrixToPyList(m)
-        return "[" .. string.gsub(m:tostring(",", ","), "i", "j") .. "]"
-    end
-    m1 = MAD.matrix(1000, 1000):seq()
-    py:send([[
-m1 = np.array(]] .. madMatrixToPyList(m1) .. [[) #Indents can be annoying...
-m1 = m1.reshape(1000, 1000)
-    ]])""")
+function madMatrixToPyList(m)
+    return "[" .. string.gsub(m:tostring(",", ","), "i", "j") .. "]"
+end
+""")
+
+matrixString = """
+    local m1 = MAD.matrix(1000, 1000):seq()
+    py:send(string.format([[
+m1 = np.array(%s).reshape(1000, 1000) #Indents can be annoying...
+    ]], madMatrixToPyList(m1)))"""
+
+mad.process.send(matrixString)
+
+cmatrixString = """
+    local cm1 = MAD.cmatrix(1000, 1000):seq()
+    py:send(string.format([[
+cm1 = np.array(%s).reshape(1000, 1000) #Indents can be annoying...
+    ]], madMatrixToPyList(cm1)))"""
+
+mad.process.send(cmatrixString)
+
+vectorString = """
+local v1 = (MAD.vector(45):seq()*2 + 1)/3
+py:send(string.format([[
+v1 = np.array(%s).reshape(45, 1)
+    ]], madMatrixToPyList(v1)))
+"""
+mad.process.send(vectorString)
+
 
 mad.process.read()
-
 print(m1)
+
+mad.process.read()
+print(cm1)
+
+mad.process.read()
+print(v1)
