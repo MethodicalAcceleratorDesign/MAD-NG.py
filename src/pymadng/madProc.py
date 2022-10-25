3fd12a17e59616602606bc9012b362e3fa35ccce
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-25 18:01:00 +0200

    Added handling of error, when ram limit is exceded sending from mad to python Formatted using black

--- src/pymadng/madProc.py
+++ src/pymadng/madProc.py
@@ -1,17 +1,14 @@
+from pickletools import pynone
 import tempfile, os, subprocess, sys, select, time
-from matplotlib import cm
 import numpy as np
 
-from pymadng.pymadClasses import madObject
-
-# Working: mad.send("""MAD.send([==[mad.send('''MAD.send([=[mad.send("MAD.send([[print('hello world')]])")]=])''')]==])""")
-# Working: mad.send("""py.send([=[mad.send("py.send([[print('hello')]])")]=])""")
+# Working: mad.send("""py:send([==[mad.send('''py:send([=[mad.send("py:send([[print('hello world')]])")]=])''')]==])""")
+# Working: mad.send("""py:send([=[mad.send("py:send([[print('hello')]])")]=])""")
 # Working for me: mad.send("""send([==[mad.send(\"\"\"send([=[mad.send("send([[print('hello world')]])")]=])\"\"\")]==])""")
 
+
 class madProcess:
-    def __init__(
-        self, pyName: str = "py", madPath: str = None, debug = False
-    ) -> None:
+    def __init__(self, pyName: str = "py", madPath: str = None, debug=False) -> None:
         self.pyName = pyName
 
         madPath = madPath or os.path.dirname(os.path.abspath(__file__)) + "/mad"
@@ -29,10 +26,12 @@ class madProcess:
         )
         os.close(pyOutput)
 
-        if self.process.poll(): # Required?
-            raise(OSError(
-                f"Unsuccessful opening of {madPath}, process closed immediately"
-            ))
+        if self.process.poll():  # Required?
+            raise (
+                OSError(
+                    f"Unsuccessful opening of {madPath}, process closed immediately"
+                )
+            )
 
         self.globalVars = {"np": np}
 
@@ -56,15 +55,21 @@ class madProcess:
         self.process.stdin.write(
             ("assert(load([==========[" + input + "]==========]))()\n").encode("utf-8")
         )
-        
+
     def rawRead(self):
         bytesToRead = int(self.fpyInput.read(10))
-        cmds =  self.fpyInput.read(bytesToRead)
+        cmds = self.fpyInput.read(bytesToRead)
+        if len(cmds) != bytesToRead:
+            raise (
+                EOFError(
+                    "Too much data attempted to be written from MAD, the amount of data exceeded your free RAM"  # Check if universal!
+                )
+            )
         return cmds
 
-    def read(self, env = {}, timeout = 10) -> dict:
+    def read(self, env={}, timeout=10) -> dict:
         if self.pyInPoll.poll(1000 * timeout) == []:  # timeout seconds poll!
-            raise(TimeoutError("No commands have been send to from MAD to Py!"))
+            raise (TimeoutError("No commands have been send to from MAD to Py!"))
         code = compile(self.rawRead(), "pyInput", "exec")
         env.update({"mad": self})
         exec(code, self.globalVars, env)
