09c0901349dbd4595f1c19246cd0865f918dde8a
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-25 17:59:27 +0200

    Fixed example so no risk of deadlock

--- examples/ex-LowLevel/ex-send-recv.py
+++ examples/ex-LowLevel/ex-send-recv.py
@@ -6,6 +6,7 @@ arr0 = np.zeros((10000, 1000)) + 1j# 2*10000*1000*8 -> 160 MB
 
 start_time = time.time()
 mad = MAD(debug = False)
+# Number one rule! If you ask mad to send you something, read it!
 
 strArr0 = arr0.tobytes()
 mad.send(f"cm1 = py:read_cmat({arr0.shape[0]}, {arr0.shape[1]}, {len(strArr0)})")
@@ -17,18 +18,25 @@ cmatrixString = """
     """
 
 mad.send(cmatrixString.format("cm4", "*", 1))
-mad.send(cmatrixString.format("cm4", "*", 1))
-mad.send(cmatrixString.format("cm4", "*", 1))
-mad.send(cmatrixString.format("cm1", "*", 2))
-mad.send(cmatrixString.format("cm2", "*", 2))
-mad.send(cmatrixString.format("cm3", "/", 3))
+cm4 = mad.read()["cm4"]
 
+# ------------------------DEADLOCK CHECK!----------------------------------------------#
+# python could be stuck writing while mad will also be stuck writing, if not handled properly.
+# mad.send(f"cm1 = py:read_cmat({arr0.shape[0]}, {arr0.shape[1]}, {len(strArr0)})")
+# mad.process.process.stdin.write(strArr0)
+# -------------------------------------------------------------------------------------#
+
+mad.send(cmatrixString.format("cm4", "*", 1))
 cm4 = mad.read()["cm4"]
+mad.send(cmatrixString.format("cm4", "*", 1))
 cm4 = mad.read()["cm4"]
-cm4 = mad.read()["cm4"]
+mad.send(cmatrixString.format("cm1", "*", 2))
 cm1 = mad.read()["cm1"]
+mad.send(cmatrixString.format("cm2", "*", 2))
 cm2 = mad.read()["cm2"]
+mad.send(cmatrixString.format("cm3", "/", 3))
 cm3 = mad.read()["cm3"]
+
 
 # # v1 = mad.read()["v1"]
 # # print(v1)
@@ -38,3 +46,15 @@ print(np.all(cm1 == arr0*2))
 print(np.all(cm2 == arr0*2*2))
 print(np.all(cm3 == arr0*2/3))
 print(np.all(cm4 == arr0))
+
+
+mad.send("""py:send([=[mad.send("py:send([[print('it worked!')]])")]=])""")
+mad.read()
+print("On second read:")
+mad.read()
+
+mad.send("""py:send([==[mad.send('''py:send([=[mad.send("py:send([[print('hello world')]])")]=])''')]==])""")
+mad.read()
+mad.read()
+print("On third read:")
+mad.read()
