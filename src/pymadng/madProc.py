7ad9e628e181c8732058d20aca0e297c779bc948
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-10-26 11:09:13 +0200

    No longer using interactive mode, each py object in mad now has it's own environment (using load) Updated to have raw send, so that sending and receiving is symmetric

--- src/pymadng/madProc.py
+++ src/pymadng/madProc.py
@@ -1,4 +1,3 @@
-from pickletools import pynone
 import tempfile, os, subprocess, sys, select, time
 import numpy as np
 
@@ -13,18 +12,19 @@ class madProcess:
 
         madPath = madPath or os.path.dirname(os.path.abspath(__file__)) + "/mad"
 
-        self.pyInput, pyOutput = os.pipe()
+        self.pyInput, madOutput = os.pipe()
+        startupChunk  = "MAD.pymad '" + pyName + "' {dispdbgf = " + str(debug).lower() + "} :publish() :open_pipe(" + str(madOutput) + ")"
 
         self.process = subprocess.Popen(
-            [madPath, "-q", "-i"],
+            [madPath, "-q", "-e", startupChunk],
             bufsize=0,
             stdout=sys.stdout,
             stderr=sys.stderr,
             stdin=subprocess.PIPE,
-            pass_fds=[pyOutput],
-            # text=True,
+            pass_fds=[madOutput],
+            # text=True, # Makes sending byte data easy
         )
-        os.close(pyOutput)
+        os.close(madOutput)
 
         if self.process.poll():  # Required?
             raise (
@@ -34,27 +34,17 @@ class madProcess:
             )
 
         self.globalVars = {"np": np}
-
-        self.send(
-            "MAD.pymad '"
-            + pyName
-            + "' {dispdbgf = "
-            + str(debug).lower()
-            + "} :publish() :open_pipe("
-            + str(pyOutput)
-            + ")"
-        )
         self.fpyInput = os.fdopen(self.pyInput, "rb")
         self.pyInPoll = select.poll()
         self.pyInPoll.register(self.pyInput, select.POLLIN)
-        self.send("_PROMPT  = ''")  # Change this to change how output works
-        self.send("_PROMPT2 = ''")  # Change this to change how output works
-        # print()
 
     def send(self, input: str):
-        self.process.stdin.write(
-            ("assert(load([==========[" + input + "]==========]))()\n").encode("utf-8")
-        )
+        self.rawSend(input.encode("utf-8"))
+
+    def rawSend(self, input: str):
+        bytesToRead = len(input)
+        self.process.stdin.write(f"{bytesToRead}".encode("utf-8"))
+        self.process.stdin.write(input)
 
     def rawRead(self):
         bytesToRead = int(self.fpyInput.read(10))
