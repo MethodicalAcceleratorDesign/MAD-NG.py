47b82e285c92fde3cfc0f43459cb50f7bafdd5da
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-12-08 14:18:57 +0100

    Added file that can run all the examples

---
+++ examples/runall.py
@@ -0,0 +1,18 @@
+import os
+current_dir = os.path.dirname(__file__) + "/"
+
+filenames = [
+    "ex-benchmark-and-fork/ex-benchmark-and-fork.py", "ex-fodo/ex-fodos.py", "ex-lhc-couplingLocal/lhc-couplingLocal.py",
+    "ex-limitations/ex-limitations.py", "ex-LowLevel/ex-send-multypes.py", "ex-LowLevel/ex-send-recv.py", 
+    "ex-ps-twiss/ps-twiss.py", "ex-recv-lhc/ex-defexpr.py"
+    ]
+for name in filenames:
+    env = {"__file__": current_dir + name}
+    with open(name, "r") as f:
+        exec(f.read(), env)
+
+
+#Cleanup tfs tables (comment out if you would like to read)
+for x in os.listdir():
+    if x.split(".")[-1] == "tfs":
+        os.remove(x)
