f565949e77898dde68d6e1c6bbac55f55d0d9e49
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-12-13 09:10:07 +0100

    update version

--- src/pymadng/__init__.py
+++ src/pymadng/__init__.py
@@ -1,7 +1,7 @@
 from .mad_object import MAD
 
 __title__ = "pymadng"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __summary__ = "Python interface to MAD-NG running as subprocess"
 __uri__ = "https://github.com/MethodicalAcceleratorDesign/MADpy"
