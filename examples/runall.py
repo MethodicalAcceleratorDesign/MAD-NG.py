6862bead9b3cf8dc4abca5ae5a7d3d746f52984a
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-12-16 14:56:27 +0100

    Renamed file

--- examples/runall.py
+++ examples/runall.py
@@ -3,7 +3,7 @@ current_dir = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 filenames = [
     "ex-benchmark-and-fork/ex-benchmark-and-fork.py", "ex-fodo/ex-fodos.py", "ex-lhc-couplingLocal/lhc-couplingLocal.py",
-    "ex-breaking-sequencing/ex-breaking-sequencing.py", "ex-LowLevel/ex-send-multypes.py", "ex-LowLevel/ex-send-recv.py", 
+    "ex-managing-refs/ex-managing-refs.py", "ex-LowLevel/ex-send-multypes.py", "ex-LowLevel/ex-send-recv.py", 
     "ex-ps-twiss/ps-twiss.py", "ex-recv-lhc/ex-defexpr.py"
     ]
 for name in filenames:
