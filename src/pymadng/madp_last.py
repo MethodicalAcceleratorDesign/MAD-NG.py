cc6e9efb7a1eb6dcd45b7716be948824f153272c
jgray-19 <jgray-19@users.noreply.github.com>
2024-11-05 11:19:46 +0100

    Improve formatting + docstring

--- src/pymadng/madp_last.py
+++ src/pymadng/madp_last.py
@@ -1,13 +1,21 @@
 class last_counter:
-  """A very simple class to keep track of the special variable named '__last__' in MAD-NG"""
-  def __init__(self, size: int):
-    self.counter = list(range(size, 0, -1))
+	"""A class to keep track of the special variable named '__last__' in MAD-NG
 
-  def get(self):
-    assert (
-        len(self.counter) > 0
-    ), "Assigned too many anonymous variables, increase num_temp_vars or assign the variables into MAD"
-    return self.counter.pop()
+	The '__last__' variable contains the last value returned by a function in MAD-NG. 
+	This allows the user to use pythonic syntax and get a return value from a function in MAD-NG, as an object in Python.
 
-  def set(self, idx):
-    self.counter.append(idx)
+	Args:
+	  size (int): The maximum number of '__last__' variables to keep track of in the MAD-NG environment
+	"""
+
+	def __init__(self, size: int):
+		self.counter = list(range(size, 0, -1))
+
+	def get(self):
+		assert (
+			len(self.counter) > 0
+		), "Assigned too many anonymous variables, increase num_temp_vars or assign the variables into MAD"
+		return self.counter.pop()
+
+	def set(self, idx):
+		self.counter.append(idx)
