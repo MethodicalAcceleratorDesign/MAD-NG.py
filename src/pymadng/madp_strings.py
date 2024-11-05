3be793eb2e0d925ba33e10da4829c5c5426bba45
jgray-19 <jgray-19@users.noreply.github.com>
2024-11-05 11:27:40 +0100

    Format strings class

--- src/pymadng/madp_strings.py
+++ src/pymadng/madp_strings.py
@@ -1,14 +1,16 @@
 from typing import Any
+
 from .madp_pymad import mad_ref
 
+
+# Keep an eye out for failures when kwargs is empty, shouldn't occur in current setup (jgray 2023)
 def format_kwargs_to_string(py_name, **kwargs):
-	# Keep an eye out for failures when kwargs is empty, shouldn't occur in current setup
 	"""Convert a keyword argument input to a string used by MAD-NG
-	
+
 	Args:
 		py_name (str): The name of the Python variable in the MAD-NG environment
 		**kwargs: The keyword arguments to be converted to a string
-	
+
 	Returns:
 		str: The string representation of the keyword arguments (may include variables that need to be sent through the pipe)
 		list: The variables to send to the MAD-NG environment
@@ -20,7 +22,7 @@ def format_kwargs_to_string(py_name, **kwargs):
 		formatted_item, var_to_send = create_mad_string(py_name, item)
 		formatted_kwargs += formatted_key + " = " + formatted_item + ", "
 		vars_to_send.extend(var_to_send)
-	
+
 	# Add the closing bracket and return
 	return formatted_kwargs + "}", vars_to_send
 
@@ -34,7 +36,7 @@ def format_args_to_string(py_name, *args):
 
 	Returns:
 		str: The string representation of the arguments (may include variables that need to be sent through the pipe)
-		list: The variables to send to the MAD-NG environment	
+		list: The variables to send to the MAD-NG environment
 	"""
 	mad_string = ""
 	vars_to_send = []
@@ -42,7 +44,7 @@ def format_args_to_string(py_name, *args):
 		formatted_arg, var_to_send = create_mad_string(py_name, arg)
 		mad_string += formatted_arg + ", "
 		vars_to_send.extend(var_to_send)
-	
+
 	# Remove the last comma and space
 	return mad_string[:-2], vars_to_send
 
