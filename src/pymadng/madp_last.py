6502454f583991aa7fba62e930b5aeec80bba4ce
jgray-19 <jgray-19@users.noreply.github.com>
2024-11-29 09:23:10 +0100

    Convert tab indentation to spaces

--- src/pymadng/madp_last.py
+++ src/pymadng/madp_last.py
@@ -1,21 +1,21 @@
 class last_counter:
-	"""A class to keep track of the special variable named '__last__' in MAD-NG
+    """A class to keep track of the special variable named '__last__' in MAD-NG
 
-	The '__last__' variable contains the last value returned by a function in MAD-NG. 
-	This allows the user to use pythonic syntax and get a return value from a function in MAD-NG, as an object in Python.
+    The '__last__' variable contains the last value returned by a function in MAD-NG. 
+    This allows the user to use pythonic syntax and get a return value from a function in MAD-NG, as an object in Python.
 
-	Args:
-	  size (int): The maximum number of '__last__' variables to keep track of in the MAD-NG environment
-	"""
+    Args:
+      size (int): The maximum number of '__last__' variables to keep track of in the MAD-NG environment
+    """
 
-	def __init__(self, size: int):
-		self.counter = list(range(size, 0, -1))
+    def __init__(self, size: int):
+        self.counter = list(range(size, 0, -1))
 
-	def get(self):
-		assert (
-			len(self.counter) > 0
-		), "Assigned too many anonymous variables, increase num_temp_vars or assign the variables into MAD"
-		return self.counter.pop()
+    def get(self):
+        assert (
+            len(self.counter) > 0
+        ), "Assigned too many anonymous variables, increase num_temp_vars or assign the variables into MAD"
+        return self.counter.pop()
 
-	def set(self, idx):
-		self.counter.append(idx)
+    def set(self, idx):
+        self.counter.append(idx)
--- src/pymadng/madp_strings.py
+++ src/pymadng/madp_strings.py
@@ -5,70 +5,70 @@ from .madp_pymad import mad_ref
 
 # Keep an eye out for failures when kwargs is empty, shouldn't occur in current setup (jgray 2023)
 def format_kwargs_to_string(py_name, **kwargs):
-	"""Convert a keyword argument input to a string used by MAD-NG
+    """Convert a keyword argument input to a string used by MAD-NG
 
-	Args:
-		py_name (str): The name of the Python variable in the MAD-NG environment
-		**kwargs: The keyword arguments to be converted to a string
+    Args:
+        py_name (str): The name of the Python variable in the MAD-NG environment
+        **kwargs: The keyword arguments to be converted to a string
 
-	Returns:
-		str: The string representation of the keyword arguments (may include variables that need to be sent through the pipe)
-		list: The variables to send to the MAD-NG environment
-	"""
-	formatted_kwargs = "{"
-	vars_to_send = []
-	for key, item in kwargs.items():
-		formatted_key = str(key).replace("'", "")
-		formatted_item, var_to_send = create_mad_string(py_name, item)
-		formatted_kwargs += formatted_key + " = " + formatted_item + ", "
-		vars_to_send.extend(var_to_send)
+    Returns:
+        str: The string representation of the keyword arguments (may include variables that need to be sent through the pipe)
+        list: The variables to send to the MAD-NG environment
+    """
+    formatted_kwargs = "{"
+    vars_to_send = []
+    for key, item in kwargs.items():
+        formatted_key = str(key).replace("'", "")
+        formatted_item, var_to_send = create_mad_string(py_name, item)
+        formatted_kwargs += formatted_key + " = " + formatted_item + ", "
+        vars_to_send.extend(var_to_send)
 
-	# Add the closing bracket and return
-	return formatted_kwargs + "}", vars_to_send
+    # Add the closing bracket and return
+    return formatted_kwargs + "}", vars_to_send
 
 
 def format_args_to_string(py_name, *args):
-	"""Convert an argument input to a string used by MAD-NG
+    """Convert an argument input to a string used by MAD-NG
 
-	Args:
-		py_name (str): The name of the Python variable in the MAD-NG environment
-		*args: The arguments to be converted to a string
+    Args:
+        py_name (str): The name of the Python variable in the MAD-NG environment
+        *args: The arguments to be converted to a string
 
-	Returns:
-		str: The string representation of the arguments (may include variables that need to be sent through the pipe)
-		list: The variables to send to the MAD-NG environment
-	"""
-	mad_string = ""
-	vars_to_send = []
-	for arg in args:
-		formatted_arg, var_to_send = create_mad_string(py_name, arg)
-		mad_string += formatted_arg + ", "
-		vars_to_send.extend(var_to_send)
+    Returns:
+        str: The string representation of the arguments (may include variables that need to be sent through the pipe)
+        list: The variables to send to the MAD-NG environment
+    """
+    mad_string = ""
+    vars_to_send = []
+    for arg in args:
+        formatted_arg, var_to_send = create_mad_string(py_name, arg)
+        mad_string += formatted_arg + ", "
+        vars_to_send.extend(var_to_send)
 
-	# Remove the last comma and space
-	return mad_string[:-2], vars_to_send
+    # Remove the last comma and space
+    return mad_string[:-2], vars_to_send
 
 
 def create_mad_string(py_name, var: Any):
-	"""Convert a list of objects into the required string for MAD-NG.
-	Converting string instead of sending more data is up to 2x faster (therefore last resort). Slowdown is mainly due to sending lists of strings.
+    """Convert a list of objects into the required string for MAD-NG.
+    Converting string instead of sending more data is up to 2x faster (therefore last resort). Slowdown is mainly due to sending lists of strings.
 
-	Args:
-		py_name (str): The name of the Python variable in the MAD-NG environment
-		var (Any): The variable to be converted to a string
-	"""
-	if isinstance(var, list):
-		string, vars_to_send = format_args_to_string(py_name, *var)
-		return "{" + string + "}", vars_to_send
-	elif var is None:
-		return "nil", []
-	elif isinstance(var, str):
-		return var, []
-	elif isinstance(var, mad_ref):
-		return var._name, []
-	elif isinstance(var, dict):
-		return format_kwargs_to_string(py_name, **var)
-	elif isinstance(var, bool):
-		return str(var).lower(), []
-	else:
-		return f"{py_name}:recv()", [var]
+    Args:
+        py_name (str): The name of the Python variable in the MAD-NG environment
+        var (Any): The variable to be converted to a string
+    """
+    if isinstance(var, list):
+        string, vars_to_send = format_args_to_string(py_name, *var)
+        return "{" + string + "}", vars_to_send
+    elif var is None:
+        return "nil", []
+    elif isinstance(var, str):
+        return var, []
+    elif isinstance(var, mad_ref):
+        return var._name, []
+    elif isinstance(var, dict):
+        return format_kwargs_to_string(py_name, **var)
+    elif isinstance(var, bool):
+        return str(var).lower(), []
+    else:
+        return f"{py_name}:recv()", [var]
