1213158c066cefb68a1786c95fccf2b1c806d4dd
Joshua Gray <61458311+jgray-19@users.noreply.github.com>
2025-04-30 15:48:30 +0200

    Update __init__ pymadng version

--- src/pymadng/__init__.py
+++ src/pymadng/__init__.py
@@ -1,7 +1,7 @@
 from .madp_object import MAD
 
 __title__ = "pymadng"
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 __summary__ = "Python interface to MAD-NG running as subprocess"
 __uri__ = "https://github.com/MethodicalAcceleratorDesign/MAD-NG.py"
